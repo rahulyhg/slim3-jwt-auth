c113609926be104c03044515c8e497be01a8c3d1
Dyer, Andy <andrew.dyer@sky.uk>
2018-11-14 14:11:52 +0000

    Added admin routes

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,7 @@
 
 use App\Controllers\AppController;
 use App\Controllers\AuthController;
+use App\Middleware\AdminMiddleware;
 use App\Middleware\AuthMiddleware;
 
 $container = $app->getContainer();
@@ -10,6 +11,10 @@ $app->get('/', AppController::class.':publicAction');
 $app->post('/login', AuthController::class.':loginAction');
 $app->post('/register', AuthController::class.':registerAction');
 
-$app->group('/auth', function () {
+$app->group('/auth', function () use ($container) {
     $this->get('', AppController::class.':privateAction');
+
+    $this->group('/admin', function () {
+        $this->get('', AppController::class.':adminAction');
+    })->add(new AdminMiddleware($container));
 })->add(new AuthMiddleware($container));
