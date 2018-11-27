f4223578e8eaae810ff5ee8fea328d05900b823d
Dyer, Andy <andrew.dyer@sky.uk>
2018-11-27 11:44:43 +0000

    Updated permission seed

--- src/Seeds/PermissionSeed.php
+++ src/Seeds/PermissionSeed.php
@@ -18,89 +18,43 @@ class PermissionSeed extends AbstractSeed
      */
     public function run()
     {
-        $this->seedPermission1()
-            ->seedPermission2()
-            ->seedPermission3()
-            ->seedPermission4()
-            ->seedPermission5()
-            ->seedPermission6();
-    }
-
-    /**
-     * @return PermissionSeed
-     */
-    private function seedPermission1(): self
-    {
-        $permission = new Permission();
-        $permission->name = Permission::DELETE_USERS;
-        $permission->description = '';
-        $permission->save();
-
-        return $this;
-    }
-
-    /**
-     * @return PermissionSeed
-     */
-    private function seedPermission2(): self
-    {
-        $permission = new Permission();
-        $permission->name = Permission::MANAGE_ROLES;
-        $permission->description = '';
-        $permission->save();
-
-        return $this;
-    }
-
-    /**
-     * @return PermissionSeed
-     */
-    private function seedPermission3(): self
-    {
-        $permission = new Permission();
-        $permission->name = Permission::EDIT_USERS;
-        $permission->description = '';
-        $permission->save();
-
-        return $this;
-    }
-
-    /**
-     * @return PermissionSeed
-     */
-    private function seedPermission4(): self
-    {
-        $permission = new Permission();
-        $permission->name = Permission::EDIT_ADMINS;
-        $permission->description = '';
-        $permission->save();
-
-        return $this;
-    }
-
-    /**
-     * @return PermissionSeed
-     */
-    private function seedPermission5(): self
-    {
-        $permission = new Permission();
-        $permission->name = Permission::VIEW_ADMIN_PAGES;
-        $permission->description = '';
-        $permission->save();
-
-        return $this;
-    }
-
-    /**
-     * @return PermissionSeed
-     */
-    private function seedPermission6(): self
-    {
-        $permission = new Permission();
-        $permission->name = Permission::MAKE_ADMIN;
-        $permission->description = '';
-        $permission->save();
-
-        return $this;
+        Permission::insert([
+            [
+                'name' => Permission::DELETE_USERS,
+                'description' => '',
+                'created_on' => '2018-10-01 14:01:15',
+                'updated_on' => '2018-10-01 14:01:15',
+            ],
+            [
+                'name' => Permission::EDIT_ADMINS,
+                'description' => '',
+                'created_on' => '2018-10-01 14:01:15',
+                'updated_on' => '2018-10-01 14:01:15',
+            ],
+            [
+                'name' => Permission::EDIT_USERS,
+                'description' => '',
+                'created_on' => '2018-10-01 14:01:15',
+                'updated_on' => '2018-10-01 14:01:15',
+            ],
+            [
+                'name' => Permission::MAKE_ADMIN,
+                'description' => '',
+                'created_on' => '2018-10-01 14:01:15',
+                'updated_on' => '2018-10-01 14:01:15',
+            ],
+            [
+                'name' => Permission::MANAGE_ROLES,
+                'description' => '',
+                'created_on' => '2018-10-01 14:01:15',
+                'updated_on' => '2018-10-01 14:01:15',
+            ],
+            [
+                'name' => Permission::VIEW_ADMIN_PAGES,
+                'description' => '',
+                'created_on' => '2018-10-01 14:01:15',
+                'updated_on' => '2018-10-01 14:01:15',
+            ],
+        ]);
     }
 }
