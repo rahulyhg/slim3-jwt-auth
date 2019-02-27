b4dc0f120d28a0300e349254650fd37edd459ac1
Dyer, Andy <andrew.dyer@sky.uk>
2019-02-27 12:02:54 +0000

    Import routes from routes directory

--- app/routes.php
+++ app/routes.php
@@ -1,20 +1,7 @@
 <?php
 
-use App\Controllers\AppController;
-use App\Controllers\AuthController;
-use App\Middleware\AdminMiddleware;
-use App\Middleware\AuthMiddleware;
-
 $container = $app->getContainer();
 
-$app->get('/', AppController::class.':publicAction');
-$app->post('/login', AuthController::class.':loginAction');
-$app->post('/register', AuthController::class.':registerAction');
-
-$app->group('/auth', function () use ($container) {
-    $this->get('', AppController::class.':privateAction');
-
-    $this->group('/admin', function () {
-        $this->get('', AppController::class.':adminAction');
-    })->add(new AdminMiddleware($container));
-})->add(new AuthMiddleware($container));
+foreach (glob(__DIR__.'/routes/*.php') as $route) {
+    require_once $route;
+}
